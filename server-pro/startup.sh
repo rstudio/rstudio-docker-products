fe4957387971093384f29a21b96ac35f23b533c6
Cole Arendt <cole@rstudio.com>
2020-03-20 18:30:17 -0400

    Make Rsp more robust (#32)
    * change process ordering
    
    * add an apt-update
    without this, things can fail if it is the first non-cached layer
    
    * add a launcher timeout defaulting to 10 seconds
    
    * change how logs are output / tailed
    
    * add LAUNCHER_TIMEOUT docs
    
    * add logging.conf
    
    * provide logging.conf to image

--- server-pro/startup.sh
+++ server-pro/startup.sh
@@ -34,8 +34,15 @@ fi
 
 
 # Start Server Pro
-/usr/lib/rstudio-server/bin/rstudio-launcher > /dev/null 2>&1 &
-wait-for-it.sh localhost:5559 -t 0
-/usr/lib/rstudio-server/bin/rserver
-wait-for-it.sh localhost:8787 -t 0
-tail -f /var/lib/rstudio-server/monitor/log/*.log /var/lib/rstudio-launcher/*.log /var/lib/rstudio-launcher/Local/*.log
+/usr/lib/rstudio-server/bin/rstudio-launcher > /var/log/rstudio-launcher.log 2>&1 &
+wait-for-it.sh localhost:5559 -t $LAUNCHER_TIMEOUT
+
+# touch log files to initialize them
+su rstudio-server -c 'touch /var/lib/rstudio-server/monitor/log/rstudio-server.log'
+touch /var/log/rstudio-server.log
+
+tail -n 100 -f /var/lib/rstudio-server/monitor/log/*.log /var/lib/rstudio-launcher/*.log /var/lib/rstudio-launcher/Local/*.log /var/log/rstudio-launcher.log /var/log/rstudio-server.log &
+
+# the main container process
+# cannot use "exec" or the "trap" will be lost
+/usr/lib/rstudio-server/bin/rserver --server-daemonize 0 > /var/log/rstudio-server.log 2>&1
