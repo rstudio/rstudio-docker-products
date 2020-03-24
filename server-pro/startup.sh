7ac735fca9af3eba30022aeb6561f1ca9141ab87
Cole Arendt <cole.arendt@outlook.com>
2020-03-23 21:42:42 -0400

    switch startup script back to normal

--- server-pro/startup.sh
+++ server-pro/startup.sh
@@ -38,8 +38,7 @@ fi
 wait-for-it.sh localhost:5559 -t $LAUNCHER_TIMEOUT
 
 # touch log files to initialize them
-touch /var/lib/rstudio-server/monitor/log/rstudio-server.log
-chown rstudio-server:rstudio-server /var/lib/rstudio-server/monitor/log/rstudio-server.log
+su rstudio-server -c 'touch /var/lib/rstudio-server/monitor/log/rstudio-server.log'
 touch /var/log/rstudio-server.log
 
 tail -n 100 -f /var/lib/rstudio-server/monitor/log/*.log /var/lib/rstudio-launcher/*.log /var/lib/rstudio-launcher/Local/*.log /var/log/rstudio-launcher.log /var/log/rstudio-server.log &
