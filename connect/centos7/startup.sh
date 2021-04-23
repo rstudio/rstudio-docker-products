af6306762b8d2400f7afb6f64b3022360c90db13
josiahparry <josiah.parry@gmail.com>
2021-04-23 09:27:50 -0400

    add centos7 image

---
+++ connect/centos7/startup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Deactivate license when it exists
+deactivate() {
+    echo "Deactivating license ..."
+    /opt/rstudio-connect/bin/license-manager deactivate >/dev/null 2>&1
+}
+trap deactivate EXIT
+
+# Activate License
+if ! [ -z "$RSC_LICENSE" ]; then
+    /opt/rstudio-connect/bin/license-manager activate $RSC_LICENSE
+elif ! [ -z "$RSC_LICENSE_SERVER" ]; then
+    /opt/rstudio-connect/bin/license-manager license-server $RSC_LICENSE_SERVER
+elif test -f "/etc/rstudio-connect/license.lic"; then
+    /opt/rstudio-connect/bin/license-manager activate-file /etc/rstudio-connect/license.lic
+fi
+
+# lest this be inherited by child processes
+unset RSC_LICENSE
+unset RSC_LICENSE_SERVER
+
+# Start RStudio Connect
+/opt/rstudio-connect/bin/connect --config /etc/rstudio-connect/rstudio-connect.gcfg
