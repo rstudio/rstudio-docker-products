f5640ea10e6d3dd7570a8d550212bfaf730ae6c9
Nahara <41586691+nahara7@users.noreply.github.com>
2021-07-20 16:26:29 -0400

    changer server startup command

--- server-pro/test/run_tests.sh
+++ server-pro/test/run_tests.sh
@@ -4,7 +4,7 @@
 cat > /tmp/startup.log
 
 # start rstudio-server
-tini -- /usr/local/bin/startup.sh 2>/tmp/startup.log &
+rstudio-server start 2>/tmp/startup.log &
 
 GOSS_FILE=${GOSS_FILE:-/tmp/goss.yaml}
 GOSS_VARS=${GOSS_VARS:-/tmp/goss_vars.yaml}
