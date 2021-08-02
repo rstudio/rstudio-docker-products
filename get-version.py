b4e742b94a875d374ec85c1d762d60c0c1bad18d
Cole Arendt <cole.arendt@outlook.com>
2021-08-02 14:13:13 -0400

    Revert "handle new version names"
    This reverts commit 13593d1b53f997a35abc2d6757108ae44b701a08.

--- get-version.py
+++ get-version.py
@@ -74,11 +74,11 @@ def rstudio_workbench_daily():
     daily_url = "https://dailies.rstudio.com/rstudioserver/pro/bionic/x86_64/"
     raw_daily = requests.get(daily_url).content
 
-    version_regex = re.compile('rstudio-workbench-(daily-)*([0-9\.\-]*)-amd64.deb')
+    version_regex = re.compile('rstudio-workbench-([0-9\.\-]*)-amd64.deb')
     version_match = version_regex.search(str(raw_daily))
 
-    # group 0 = whole match, group 1 = optional "daily-", 2 = version capture group
-    return version_match.group(2)
+    # group 0 = whole match, group 1 = first capture group
+    return version_match.group(1)
 
 
 def get_downloads_json():
