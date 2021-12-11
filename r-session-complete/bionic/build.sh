36e1c3edd021fdd4037839e99905395d9b4aa653
Michael Mayer <michael.mayer@rstudio.com>
2021-12-11 20:07:45 +0000

    add slurm stuff to singularity file and set TMPDIR to /scratch/tmp for build

--- r-session-complete/bionic/build.sh
+++ r-session-complete/bionic/build.sh
@@ -1,9 +1,9 @@
-VERSION=0.2
+VERSION=0.4
 docker build -f Dockerfile . -t r-session-complete:${VERSION}-bionic
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 docker tag r-session-complete:${VERSION}-bionic localhost:5000/r-session-complete:${VERSION}-bionic 
 docker push localhost:5000/r-session-complete:${VERSION}-bionic 
-export TMP=/efs/tmp
+export TMP=/scratch/tmp
 export TEMP=$TMP
 export TMPDIR=$TMP
 export TEMPDIR=$TMP
