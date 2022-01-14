002d4f964b1d1065fb277b9a730581d0690349ca
Cole Arendt <cole.arendt@outlook.com>
2022-01-14 12:39:09 -0500

    add azure-ml copy of build from rstudio-workbench

---
+++ helper/workbench-for-microsoft-azureml/install-r.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -xe -o pipefail
+
+install_r_packages() {
+  # given a one-per-line file of R packages, parses the file and installs those R
+  # packages to the provided (or default) R installation.
+
+  set -xe
+
+  local UBUNTU_CODENAME="bionic"
+
+  # passing a r binary as second arg will install with that R version
+  local R_BIN=${2:-"/usr/lib/R/bin/R"}
+
+  # passing a CRAN repo as third arg will install from that repo, in this case however,
+  # we are using the RStudio public package manager so we can install from binaries and not source
+  # this speeds everything up dramatically
+  local CRAN_REPO=${3:-"https://packagemanager.rstudio.com/cran/__linux__/${UBUNTU_CODENAME}/latest"}
+
+  # create an R matrix-style string of packages
+  local r_packages=`awk '{print "\"" $0 "\""}' $1 | paste -d',' -s  -`
+
+  # install packages enumerated in the file to the R binary passed
+  pp "Installing R packages for $R_BIN"
+  $R_BIN --slave -e "install.packages(c(${r_packages}), repos = \"${CRAN_REPO}\")" > /dev/null
+}
+
+
+for rvers in 3.3.3 3.4.4 3.5.3 3.6.3 4.0.5 4.1.2; do
+    # install r version
+    curl -O https://cdn.rstudio.com/r/ubuntu-1804/pkgs/r-${rvers}_1_amd64.deb
+    DEBIAN_FRONTEND=noninteractive gdebi --non-interactive r-${rvers}_1_amd64.deb
+    rm -f ./r-${R_VERSION_ALT}_1_amd64.deb
+
+    # install packages
+    install_packages /tmp/package_list.txt /opt/R/${rvers}/bin/R https://packagemanager.rstudio.com/cran/__linux__/bionic/latest
+done
+
+rm -f /tmp/package_list.txt
---
+++ helper/workbench-for-microsoft-azureml/test/run_tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -xe
+
+RSW_TIMEOUT=${RSW_TIMEOUT:-15}
+
+touch /tmp/startup.log
+trap 'err=$?; echo >&2 "run_tests.sh encountered an error: $err"; cat /tmp/startup.log; exit $err' ERR
+
+# start rstudio-server
+echo "--> Starting RStudio Workbench"
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf > /tmp/startup.log 2>&1 &
+
+echo "--> Waiting for workbench to startup... with RSW_TIMEOUT: $RSW_TIMEOUT"
+wait-for-it.sh localhost:8787 -t $RSW_TIMEOUT
+echo "--> Startup complete"
+
+GOSS_FILE=${GOSS_FILE:-/tmp/goss.yaml}
+GOSS_VARS=${GOSS_VARS:-/tmp/goss_vars.yaml}
+GOSS_VERSION=${GOSS_VERSION:-0.3.16}
+GOSS_MAX_CONCURRENT=${GOSS_MAX_CONCURRENT:-50}
+
+# default to empty var file (since vars are not necessary)
+if [ ! -f "$GOSS_VARS" ]; then
+  touch $GOSS_VARS
+fi
+
+# install goss to tmp location and make executable
+curl -sL https://github.com/aelsabbahy/goss/releases/download/v$GOSS_VERSION/goss-linux-amd64 -o /tmp/goss \
+  && chmod +x /tmp/goss \
+  && GOSS=/tmp/goss
+
+GOSS_FILE=$GOSS_FILE GOSS_VARS=$GOSS_VARS $GOSS v --format documentation --max-concurrent $GOSS_MAX_CONCURRENT
