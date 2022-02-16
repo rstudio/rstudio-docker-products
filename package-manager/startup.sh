a4b53a58aa82a049cd1cb6a8b9e43a6aca5240a5
Aaron Jacobs <aaron.jacobs@rstudio.com>
2022-02-16 16:34:59 -0500

    Make it possible to run the RSPM image as a non-root user.
    The official Ubuntu packages for RSPM set up an 'rstudio-pm' user to run
    the service. However, the existing Docker image could not be run as this
    user for two reasons:
    
    * The existing Dockerfile creates some directories required at runtime
      as root; and
    
    * The existing installation step does not use userspace license
      activation [0], so later attempts to activate a license as a non-root
      user will fail.
    
    This commit fixes both these issues. Images still default to running as
    root (for now, to preserve compatibility), but they can also be run as
    'rstudio-pm' now as well.
    
    [0]: https://docs.rstudio.com/rspm/admin/appendix/minimal-root-use-installation/#licensing
    
    Signed-off-by: Aaron Jacobs <aaron.jacobs@rstudio.com>

--- package-manager/startup.sh
+++ package-manager/startup.sh
@@ -6,18 +6,18 @@ set -x
 # Deactivate license when it exists
 deactivate() {
     echo "Deactivating license ..."
-    /opt/rstudio-pm/bin/license-manager deactivate >/dev/null 2>&1
+    /opt/rstudio-pm/bin/license-manager deactivate --userspace >/dev/null 2>&1
 }
 trap deactivate EXIT
 
 # Activate License
 RSPM_LICENSE_FILE_PATH=${RSPM_LICENSE_FILE_PATH:-/etc/rstudio-pm/license.lic}
 if ! [ -z "$RSPM_LICENSE" ]; then
-    /opt/rstudio-pm/bin/license-manager activate $RSPM_LICENSE
+    /opt/rstudio-pm/bin/license-manager activate $RSPM_LICENSE --userspace
 elif ! [ -z "$RSPM_LICENSE_SERVER" ]; then
-    /opt/rstudio-pm/bin/license-manager license-server $RSPM_LICENSE_SERVER
+    /opt/rstudio-pm/bin/license-manager license-server $RSPM_LICENSE_SERVER --userspace
 elif test -f "$RSPM_LICENSE_FILE_PATH"; then
-    /opt/rstudio-pm/bin/license-manager activate-file $RSPM_LICENSE_FILE_PATH
+    /opt/rstudio-pm/bin/license-manager activate-file $RSPM_LICENSE_FILE_PATH --userspace
 fi
 
 # ensure these cannot be inherited by child processes
