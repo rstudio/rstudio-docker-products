616ee942b25c49ae5b765fdd50a07948f0dee9f4
dkasabovn <dkasabovn@gmail.com>
2022-07-11 16:54:20 -0700

    new workflows

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-docker buildx build --tag rstudio/testing \
-	--build-arg RSW_VERSION=`just gv workbench --type=release --local` \
-	--build-arg RSW_DOWNLOAD_URL=https://s3.amazonaws.com/rstudio-ide-build/server/bionic/x86_64 \
-	--file=./workbench/docker/bionic/Dockerfile workbench
