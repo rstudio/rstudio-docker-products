c2fdab0e0291be2bebe9c041d0889cbb226b377e
Ian Pittwood <ian.pittwood@rstudio.com>
2023-04-12 12:50:47 -0700

    Add a variety of basic data science packages into the image

---
+++ workbench-for-google-cloud-workstations/deps/install_r_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+UBUNTU_CODENAME=$(lsb_release -cs)
+CRAN_REPO="https://packagemanager.rstudio.com/cran/__linux__/${UBUNTU_CODENAME}/latest"
+
+r_packages=$(awk '{print "\"" $0 "\""}' r_packages.txt | paste -d',' -s  -)
+/opt/R/${R_VERSION}/bin/R --slave -e "install.packages(c(${r_packages}), repos = \"${CRAN_REPO}\")"
+/opt/R/${R_VERSION_ALT}/bin/R --slave -e "install.packages(c(${r_packages}), repos = \"${CRAN_REPO}\")"
