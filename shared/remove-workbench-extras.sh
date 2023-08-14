9e48c681371ae7a2d05b1fa956a8bee5594f2abc
Maria Semple <maria@rstudio.com>
2023-08-14 16:48:16 -0700

    add a script that removes unused workbench components

---
+++ shared/remove-workbench-extras.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rm -rf /var/lib/rstudio-server/r-versions
+rm /usr/lib/rstudio-server/bin/rserver-saml
+rm /usr/lib/rstudio-server/bin/rserver-openid
+rm /usr/lib/rstudio-server/bin/librclient.so
+rm /usr/lib/rstudio-server/bin/librserver.so
+rm /usr/lib/rstudio-server/bin/license-manager
+rm /usr/lib/rstudio-server/bin/license-manager.conf
+rm /usr/lib/rstudio-server/bin/locktester
+rm /usr/lib/rstudio-server/bin/migrate.sh
+rm /usr/lib/rstudio-server/bin/pamtester 
+rm /usr/lib/rstudio-server/bin/rpcsend
+rm /usr/lib/rstudio-server/bin/rserver
+rm /usr/lib/rstudio-server/bin/rserver-acls
+rm /usr/lib/rstudio-server/bin/rserver-admin
+rm /usr/lib/rstudio-server/bin/rserver-db
+rm /usr/lib/rstudio-server/bin/rserver-encrypt
+rm /usr/lib/rstudio-server/bin/rserver-http
+rm /usr/lib/rstudio-server/bin/rserver-monitor
+rm /usr/lib/rstudio-server/bin/rserver-pam
+rm /usr/lib/rstudio-server/bin/rserver-session-reaper
+rm /usr/lib/rstudio-server/bin/rstudio-kubernetes-launcher
+rm /usr/lib/rstudio-server/bin/rstudio-launcher
+rm /usr/lib/rstudio-server/bin/rstudio-launcher.sh
+rm /usr/lib/rstudio-server/bin/rstudio-local-launcher
+rm /usr/lib/rstudio-server/bin/rstudio-server
+rm /usr/lib/rstudio-server/bin/rstudio-slurm-launcher
+rm /usr/lib/rstudio-server/bin/run-diagnostics
+rm -rf /usr/lib/rstudio-server/www-admin
+rm -rf /usr/lib/rstudio-server/www-launcher
+rm -rf /usr/lib/rstudio-server/www-workspaces
+rm -rf /usr/lib/rstudio-server/db
+rm -rf /usr/lib/rstudio-server/conf
+rm -rf /usr/lib/rstudio-server/extras
