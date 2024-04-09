74e3f54acb3136705eb3408a1f13f69db9465f30
Ian Pittwood <ian.pittwood@rstudio.com>
2024-04-09 09:59:55 -0700

    Add wait time in Workbench test runner to ensure services are up

--- workbench/test/run_tests.sh
+++ workbench/test/run_tests.sh
@@ -9,15 +9,17 @@ trap 'err=$?; echo >&2 "run_tests.sh encountered an error: $err"; cat /tmp/start
 # start rstudio-server
 echo "--> Starting RStudio Workbench"
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf > /tmp/startup.log 2>&1 &
+sleep 5
 
 echo "--> Waiting for workbench to startup... with RSW_TIMEOUT: $RSW_TIMEOUT"
 wait-for-it.sh localhost:8787 -t $RSW_TIMEOUT
 wait-for-it.sh localhost:5559 -t $RSW_TIMEOUT
 echo "--> Startup complete"
+sleep 5
 
 GOSS_FILE=${GOSS_FILE:-/tmp/test/goss.yaml}
 GOSS_VERSION=${GOSS_VERSION:-0.4.6}
-GOSS_MAX_CONCURRENT=${GOSS_MAX_CONCURRENT:-50}
+GOSS_MAX_CONCURRENT=${GOSS_MAX_CONCURRENT:-10}
 
 # install goss to tmp location and make executable
 curl -sL https://github.com/aelsabbahy/goss/releases/download/v$GOSS_VERSION/goss-linux-amd64 -o /tmp/goss \
