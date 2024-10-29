d340c7a8db106c034b365e3ffe3495fab4a18323
Skye Turriff <turriff.skye@gmail.com>
2024-10-28 21:55:17 -0400

    add bake preview prototype for workbench session init container

---
+++ workbench-session-init/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# The target should exist and be an empty directory.
+T=/mnt/init
+
+if [ ! -d "${T}" ] ; then
+    echo "Cannot find the copy target ${T}"
+    exit 1
+fi
+
+echo "Copying files from /session-components to /mnt/init"
+time cp -r /session-components/* /mnt/init
