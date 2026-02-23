1c300358350ef3943f612221bd0a57b63097353e
edavidaja <david@posit.co>
2026-02-23 13:00:20 -0500

    remove virtualenv

--- product/base/scripts/ubuntu/install_python.sh
+++ product/base/scripts/ubuntu/install_python.sh
@@ -97,7 +97,6 @@ install_python() {
     $PYTHON_BIN -m ensurepip --upgrade
     $PYTHON_BIN -m pip install -U setuptools
     $PYTHON_BIN -m pip install -U pip
-    $PYTHON_BIN -m pip install -U virtualenv
 }
 
 install_python_packages() {
